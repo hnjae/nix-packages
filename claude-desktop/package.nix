e98e4f40f33e98929169026ee4b55bb1aed9d82d
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-09-05 04:18:43 +0900

    chore(claude-desktop): update package

--- claude-desktop/package.nix
+++ claude-desktop/package.nix
@@ -33,7 +33,7 @@
 }:
 let
   appId = "com.anthropic.Claude";
-  version = "1.40609.0";
+  version = "1.46388.2";
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "claude-desktop";
@@ -41,7 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_${version}_amd64.deb";
-    hash = "sha256-qW6W/4601Nf/p4Wrp/wj+GhLEqyD7S70Bg8PCfQXepg=";
+    hash = "sha256-mL9U6F5JFgaMQoFFmw8EMdj/aANHc/PumDEdcgZWarE=";
   };
 
   nativeBuildInputs = [
